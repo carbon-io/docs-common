f80b48c0dc9a23338aa6589338ab3da3eddaaee2
Greg Banks <gbanks@gmail.com>
2017-04-05 18:13:42 -0700

    add todo extension to config

--- conf.py
+++ conf.py
@@ -33,6 +33,7 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.ifconfig',
     'sphinx.ext.autosectionlabel',
+    'sphinx.ext.todo'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
